150f6593760ad3b01606fba790ec3e8b0a32cec3
shayro12 <shayroditi@gmail.com>
2024-03-21 18:21:29 +0200

    init

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -1,4 +1,8 @@
 data "aws_availability_zones" "available" {}
+data "aws_caller_identity" "current" {}
+output "is_localstack" {
+  value = data.aws_caller_identity.current.id == "000000000000"
+}
 
 locals {
   name     = "test-autoscaling"
