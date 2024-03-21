91f62177ba2ed20bac6c63e5e33e1bd5e2f08b30
shayro12 <shayroditi@gmail.com>
2024-03-21 18:51:11 +0200

    debug test with localstack

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -67,9 +67,5 @@ module "ecs-service-autoscaling" {
   ecs_cluster_name = module.cluster.aws_ecs_cluster_cluster_name
   ecs_service_name = module.service.aws_ecs_service_service_name
   queue_name       = module.sqs.queue_name
-  # region                                      = "us-east-1"
-  # availability_zones                          = ["us-east-1a", "us-east-1b", "us-east-1c", "us-east-1d"]
-  # vpc_cidr_block                              = "192.168.0.0/16"
-  # public_subnets_cidrs_per_availability_zone  = ["192.168.0.0/19", "192.168.32.0/19", "192.168.64.0/19", "192.168.96.0/19"]
-  # private_subnets_cidrs_per_availability_zone = ["192.168.128.0/19", "192.168.160.0/19", "192.168.192.0/19", "192.168.224.0/19"]
+
 }
