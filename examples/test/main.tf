75cf9b08774518e4b2520c6c1c3a2f929b918252
shayro12 <shayroditi@gmail.com>
2024-03-21 19:17:00 +0200

    debug test with localstack

--- examples/test/main.tf
+++ examples/test/main.tf
@@ -52,7 +52,6 @@ module "service" {
   private_subnets     = module.vpc.private_subnets
   container_name      = "test"
   enable_autoscaling  = false
-  custom_lb_arn       = "no alb"
 }
 module "sqs" {
   source  = "terraform-aws-modules/sqs/aws"
